03fe3e6ad83435ceb31850bdc7befd284282d37a
Nikhil Chhabra <91406971+procrastinatedbread@users.noreply.github.com>
2023-06-24 02:25:20 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+<div>
+<img src="https://picsum.photos/250/250"/>
+<h3>Content</h3>
+<h2>PRODUCT NAME</h2>
+ <h3>Rs.200 <s>Rs.490</s></h3>
+ <button>Buy Now</button>
+  
+</div>
+`;
